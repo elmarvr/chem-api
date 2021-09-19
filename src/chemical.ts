451d1ef905d449ed93b207720f5a6f8156440454
elmarvr <elmarvriet@gmail.com>
2021-09-19 23:24:05 +0200

    test

---
+++ src/chemical.ts
@@ -0,0 +1,40 @@
+import express, { Router } from "express";
+import session from "express-session";
+//@ts-ignore
+import serverless from "serverless-http";
+import { chemicalController } from "./controllers/chemical.controller";
+
+const port = 3000;
+
+const app = express();
+const router = Router();
+
+app.use(
+  session({ secret: "chemistry", cookie: { maxAge: 60000, httpOnly: false } })
+);
+
+app.set("view engine", "pug");
+app.set("views", "./src/pages");
+
+router.get("/", (req, res) => {
+  res.send("test");
+});
+
+router.get("/safety", (req, res) => {
+  if (!req.session.info) {
+    res.redirect("/");
+    return;
+  }
+  res.render("safety", req.session.info);
+});
+
+router.use("/api", chemicalController);
+
+app.use("/.netlify/functions/chemical", router);
+
+const handler = serverless(app);
+
+export { handler };
+
+//TODO: remove session
+//TODO: style index page
